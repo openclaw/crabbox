7d2a20bfcb961b3e42e5d993825b4a407d72ca7a
Peter Steinberger <steipete@gmail.com>
2026-08-25 09:35:01 -0700

    test(aws): allow race readiness completion

--- internal/providers/aws/provider_test.go
+++ internal/providers/aws/provider_test.go
@@ -65,7 +65,7 @@ func TestAWSLinuxReadinessWaitsForCurrentBootCloudInit(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Cleanup(func() { _ = startedReader.Close() })
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 	cmd := exec.CommandContext(ctx, "sh", "-c", command)
 	cmd.Env = append(os.Environ(), "PATH="+dir+string(os.PathListSeparator)+os.Getenv("PATH"))
